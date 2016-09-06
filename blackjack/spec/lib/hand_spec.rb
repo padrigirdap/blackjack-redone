d25fcdd387f5b2c9604f123c0ebffa12127d396d
Padrig Tuck <Padrig@gmail.com>
2016-09-05 22:28:54 -0400

    tests for blackjack

--- blackjack/spec/lib/hand_spec.rb
+++ blackjack/spec/lib/hand_spec.rb
@@ -1,5 +1,17 @@
 require "spec_helper"
 
 RSpec.describe Hand do
-  # Your tests here
+  let(:deal1) { Hand.new }
+
+  describe ".new" do
+    it "should return a Hand object" do
+      expect(deal1.class).to eq(Hand)
+    end
+
+    it "should return an empty array" do
+      expect(deal1.hand.class).to eq(Array)
+      expect(deal1.hand.empty?).to eq(true)
+      expect(deal1.hand.size).to eq(0)
+    end
+  end
 end
